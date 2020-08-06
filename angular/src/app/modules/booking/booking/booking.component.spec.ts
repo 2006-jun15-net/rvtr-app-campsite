1859582d63ee38c30e152093e3a4abe75d2ec552
Dusten Knull <dakatk97@gmail.com>
2020-08-06 11:48:58 -0500

    Updated unit tests, style for booking components

--- angular/src/app/modules/booking/booking/booking.component.spec.ts
+++ angular/src/app/modules/booking/booking/booking.component.spec.ts
@@ -1,13 +1,68 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookingComponent } from './booking.component';
+import { SearchResultsComponent } from '../search-results/search-results.component';
+import { LodgingService } from 'src/app/services/lodging/lodging.service';
+import { of } from 'rxjs';
 
 describe('BookingComponent', () => {
+  const lodgingServiceStub = {
+    get() {
+      const lodgings = [
+        {
+          id: '',
+          location: {
+            id: '',
+            address: {
+              id: '',
+              city: '',
+              country: '',
+              postalCode: '',
+              stateProvince: '',
+              street: '',
+            },
+            locale: '',
+            latitude: '',
+            longitude: '',
+          },
+          name: '',
+          rentals: [
+            {
+              id: '',
+              name: '',
+              rentalUnit: {
+                id: '',
+                name: '',
+                occupancy: 1,
+                type: '',
+                bedrooms: [],
+                bathrooms: [],
+              },
+            },
+          ],
+          reviews: [
+            {
+              id: '',
+              accountId: '',
+              hotelId: '',
+              comment: '',
+              dateCreated: new Date(Date.now()),
+              rating: 5,
+            },
+          ],
+        },
+      ];
+
+      return of(lodgings);
+    },
+  };
+
   let component: BookingComponent;
   let fixture: ComponentFixture<BookingComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [BookingComponent],
+      declarations: [BookingComponent, SearchResultsComponent],
+      providers: [{ provide: LodgingService, useValue: lodgingServiceStub }],
     }).compileComponents();
   }));
 
