c0bdfe58da15591be941f9d3b9fba1f48350a997
Dusten Knull <dakatk97@gmail.com>
2020-08-06 09:40:56 -0500

    Added mock data to search results tests

--- angular/src/app/modules/booking/search-results/search-results.component.spec.ts
+++ angular/src/app/modules/booking/search-results/search-results.component.spec.ts
@@ -1,10 +1,56 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchResultsComponent } from './search-results.component';
+import { compileComponentFromMetadata } from '@angular/compiler';
+import { Lodging } from 'src/app/data/lodging.model';
 
 describe('SearchResultsComponent', () => {
   let component: SearchResultsComponent;
   let fixture: ComponentFixture<SearchResultsComponent>;
+  const lodgings: Lodging[] = [
+    {
+      id: '',
+      location: {
+        id: '',
+        address: {
+          id: '',
+          city: '',
+          country: '',
+          postalCode: '',
+          stateProvince: '',
+          street: '',
+        },
+        locale: '',
+        latitude: '',
+        longitude: '',
+      },
+      name: '',
+      rentals: [
+        {
+          id: '',
+          name: '',
+          rentalUnit: {
+            id: '',
+            name: '',
+            occupancy: 1,
+            type: '',
+            bedrooms: [],
+            bathrooms: [],
+          },
+        },
+      ],
+      reviews: [
+        {
+          id: '',
+          accountId: '',
+          hotelId: '',
+          comment: '',
+          dateCreated: new Date(Date.now()),
+          rating: 5,
+        },
+      ],
+    },
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,6 +61,7 @@ describe('SearchResultsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchResultsComponent);
     component = fixture.componentInstance;
+    component.lodgings = lodgings;
     fixture.detectChanges();
   });
 
