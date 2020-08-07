df00662974eac11b2ebe24645ea8c7daa448e177
kirtipatel98 <kirti.bapa@gmail.com>
2020-08-07 13:38:33 -0700

    testing lodging component

--- angular/src/app/modules/lodging/lodging/lodging.component.spec.ts
+++ angular/src/app/modules/lodging/lodging/lodging.component.spec.ts
@@ -1,21 +1,54 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LodgingComponent } from './lodging.component';
+import { Lodging } from 'src/app/data/lodging.model';
+import { of } from 'rxjs';
+import { LodgingService } from 'src/app/services/lodging/lodging.service';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LodgingComponent', () => {
   let component: LodgingComponent;
   let fixture: ComponentFixture<LodgingComponent>;
 
+  const lodging: Lodging = 
+    {
+      id: '1',
+      location: {
+        id: '1',
+        address: {
+          id: '1',
+          city: 'testCity',
+          country: 'testCountry',
+          postalCode: 'testCode',
+          stateProvince: 'testState',
+          street: 'testStreet',
+        },
+        latitude: 'testLat',
+        locale: 'testLocale',
+        longitude: 'testLong',
+      },
+      name: 'test',
+      rentals: [],
+      reviews: [],
+      bathrooms: []
+    };
+
   beforeEach(async(() => {
+
+    const lodgingService = jasmine.createSpyObj('LodgingService', ['get']);
+    lodgingService.get.and.returnValue(of(lodging));
+
     TestBed.configureTestingModule({
       declarations: [LodgingComponent],
+      imports: [RouterModule.forRoot([]), HttpClientModule],
+      providers: [{ provide: LodgingService, useValue: lodgingService }],
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(LodgingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
