047e8a0e96abe4bb0ff3399b1c5ba0d640be752c
Jack Pflug <jack.pflug4@gmail.com>
2020-08-10 17:56:51 -0400

    added some tests for newpayment component

--- angular/src/app/modules/account/newpaymentform/newpaymentform.component.spec.ts
+++ angular/src/app/modules/account/newpaymentform/newpaymentform.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewpaymentformComponent } from './newpaymentform.component';
+import { FormControl } from '@angular/forms';
+import { componentFactoryName } from '@angular/compiler';
 
 describe('NewpaymentformComponent', () => {
   let component: NewpaymentformComponent;
@@ -8,9 +10,8 @@ describe('NewpaymentformComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewpaymentformComponent ]
-    })
-    .compileComponents();
+      declarations: [NewpaymentformComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +23,26 @@ describe('NewpaymentformComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not show modal on init', () => {
+    component.ngOnInit();
+    expect(component.showModal).toBeFalsy();
+  });
+
+  it('should emit reply action on submit'),
+    () => {
+      const newform = new NewpaymentformComponent();
+      newform.PaymentForm.setValue({
+        Bank: 'Amex',
+        CCNumber: '123456781234',
+        ExpDate: '12/20',
+        SecurityNumber: '111',
+      });
+
+      newform.onSubmit();
+      expect(component.Bank).toEqual(new FormControl('Amex'));
+      expect(component.CCNumber).toEqual(new FormControl('123456781234'));
+      expect(component.ExpDate).toEqual(new FormControl('12/20'));
+      expect(component.SecurityNumber).toEqual(new FormControl('111'));
+    };
 });
